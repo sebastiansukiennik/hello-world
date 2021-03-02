e721affc7e0f2e7aa065d4d9f61c3f558aab04e9
Sebastian <ssukiennik@student.agh.edu.pl>
2021-03-02 11:21:39 +0100

    Initial Commit

---
+++ 123/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  123
+//
+//  Created by Sebastian Sukiennik on 02/03/2021.
+//  Copyright © 2021 Sebastian Sukiennik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
