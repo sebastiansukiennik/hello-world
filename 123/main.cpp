550aadb1b44925dd1175d002a6b2c1d9849d7657
Sebastian <ssukiennik@student.agh.edu.pl>
2021-03-02 11:51:25 +0100

    Henlo World wygląda lepiej

--- 123/main.cpp
+++ 123/main.cpp
@@ -11,7 +11,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    std::cout << "Hello, World!\n";
-    std::cout << "Hello, World!\n";
+    std::cout << "Henlo, World!\n";
     return 0;
 }
